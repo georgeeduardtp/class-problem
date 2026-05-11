6d945c9ea923547853b4356f22cddf4636123389
georgeeduardtp <georgeeduard.tp@gmail.com>
2026-05-11 08:39:22 +0200

    initial commit

---
+++ src/test/java/com/aula/classproblem/ClassproblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula.classproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassproblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
